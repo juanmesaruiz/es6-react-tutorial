942ce55fd18201f5195edcbf45d92767de07fd67
JUAN MESA RUIZ <juan.mesa@mango.com>
2017-08-10 16:59:22 +0200

    Fixed Project2

---
+++ Project-1/hello-world/src/components/Intro.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Intro extends Component{
+    render() {
+        return (
+            <p className="App-intro">
+                To get started, edit <code>src/App.js</code> and save to reload.
+            </p>
+        );
+    }
+}
--- Project-2/component-basic/src/App.js
+++ Project-2/component-basic/src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.yourname= "Ammy";
+        this.state = {};
+    }
+
+    sayhello(name) {
+        return "Hello " + name;
+    }
   render() {
+      const myName = 'John';
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <h2>Just some sample data: {myName}</h2>
+          <h2>Just some sample data: {this.sayhello("John")}</h2>
+          <h2>Just some sample data: {this.yourname}</h2>
       </div>
     );
   }
